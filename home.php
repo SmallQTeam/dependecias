9cd88bfc342723d92ce407246f11b1d4de45800e
SmallQTeam <122774474+SmallQTeam@users.noreply.github.com>
2023-01-24 10:48:46 +0000

    dbs

---
+++ home.php
@@ -0,0 +1,7 @@
+include('config.php');
+include('session.php');
+$userDetails=$userClass->userDetails($session_uid);
+?>
+<h1>Welcome <?php echo $userDetails->name; ?></h1>
+
+<h4><a href="<?php echo BASE_URL; ?>logout.php">Logout</a></h4>
